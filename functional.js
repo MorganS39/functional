7f11464737be5a7050d069ba28ef283044c7ce45
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-04 12:20:04 -1000

    Add elements using concat instead of push

--- functional.js
+++ functional.js
@@ -180,4 +180,12 @@ var first = [1, 2, 3];
 var second = [4, 5];
 nonMutatingConcat(first, second);
 
+// Add elements to the end of an array using concat instead of push
+function nonMutatingPush(original, newItem) {
+  return original.concat(newItem);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingPush(first, second);
+
 //
