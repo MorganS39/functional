fa1211b19236adf30a0dec812dc32f38fd19024d
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-04 12:18:18 -1000

    Combine arrays using concat

--- functional.js
+++ functional.js
@@ -173,3 +173,11 @@ var inputCities = ["Chicago", "Delhi", "Islamabad", "London", "Berlin"];
 nonMutatingSplice(inputCities);
 
 // Combine two arrays using the concat method
+function nonMutatingConcat(original, attach) {
+  return original.concat(attach);
+}
+var first = [1, 2, 3];
+var second = [4, 5];
+nonMutatingConcat(first, second);
+
+//
