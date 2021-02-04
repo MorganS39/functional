cd8bfa3593625d398ba7dd69b678524f13465634
Grumpybear39 <56695599+Grumpybear39@users.noreply.github.com>
2021-02-04 11:44:24 -1000

    Implement map on a prototype

--- functional.js
+++ functional.js
@@ -118,3 +118,16 @@ const ratings = watchList.map(item => ({
 }));
 
 // Implement map on a prototype
+var s = [23, 65, 98, 5];
+
+Array.prototype.myMap = function(callback) {
+  var newArray = [];
+  this.forEach(a => newArray.push(callback(a)));
+  return newArray;
+};
+
+var new_s = s.myMap(function(item) {
+  return item * 2;
+});
+
+// Use the filter method to extract data from an array
